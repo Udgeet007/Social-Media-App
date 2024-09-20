d3ab128ec03c6ae8861a87ea2c7a3e03daf3a596
Udgeet <bhatt8udgeet@gmail.com>
2024-09-20 12:48:32 +0530

    Top-Bar Component done.

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./topbar.css";
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">CliqueSpace</span>
+        <span className="Logo">CliqueSpace</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-      <SearchIcon/>
-      <input placeholder="Search for friend, post or video" className="searchInput"/>
+          <SearchIcon className="searchIcon"/>
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
       </div>
       <div className="topbarRight">
@@ -22,12 +27,19 @@ export default function Topbar() {
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
-            <PersonIcon/>
-            <span className="topbarIconBadge">
-              1
-            </span>
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
           </div>
         </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
       </div>
     </div>
   );
